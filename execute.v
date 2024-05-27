e6f99b774da4e09a8a3418f4dcdd1c618ec700bc
tanawin <tanawin1701d@outlook.com>
2024-05-27 20:51:49 +0700

    fix execute unit bug with missignaling and state declaration

--- execute.v
+++ execute.v
@@ -66,13 +66,18 @@ input wire[XLEN-1: 0]      mem_radData,
 input wire[REG_IDX-1 : 0] bp_idx,
 input wire[XLEN-1 : 0]    bp_val,
 
-input  wire[XLEN-1 : 0]    reg1_readData,
-input  wire[XLEN-1 : 0]    reg2_readData,
+input  wire[XLEN-1 : 0]    regFile1_readData,
+input  wire[XLEN-1 : 0]    regFile2_readData,
+
+input  wire[XLEN-1 : 0]    wb_cur_val,
+
+
 
 output reg                wb_valid,   ///// act as wire
 output reg[REG_IDX-1 : 0] wb_idx,     ///// act as wire
 output reg[XLEN-1 : 0]    wb_val,     ///// act as wire 
-output reg                wb_en_meta,      ///// act as wire valid to valid and idx
+output reg                wb_en_valid,      ///// act as wire valid to valid and idx
+output reg                wb_en_idx,
 output reg                wb_en_data,      ///// act as wire valid to valid and idx
 
 
@@ -100,7 +105,6 @@ output reg                 r2_write_en,
 output wire                      curPipReadyToRcv,
 output wire                      curPipReadyToSend
 
-
 );
 
 
@@ -109,17 +113,17 @@ output wire                      curPipReadyToSend
 
 
 
-reg[6-1: 0] pipState;
+reg[8-1: 0] pipState;
 
-parameter idleState       = 6'b000000;
-parameter waitBefState    = 6'b000001;
-parameter regAccess       = 6'b000010;
-parameter simpleExec      = 6'b000100;
-parameter shiftLeftReg    = 6'b001000;
-parameter shiftRightReg   = 6'b001000;
-parameter shiftRightArReg = 6'b001000;
-parameter ldstReg         = 6'b010000;
-parameter waitSendState   = 6'b100000;
+parameter idleState       = 8'b00000000;
+parameter waitBefState    = 8'b00000001;
+parameter regAccess       = 8'b00000010;
+parameter simpleExec      = 8'b00000100;
+parameter shiftLeftReg    = 8'b00001000;
+parameter shiftRightReg   = 8'b00010000;
+parameter shiftRightArReg = 8'b00100000;
+parameter ldstReg         = 8'b01000000;
+parameter waitSendState   = 8'b10000000;
 
 assign curPipReadyToRcv  = (pipState == waitBefState) | (curPipReadyToSend && nextPipReadyToRcv);
 
@@ -128,16 +132,16 @@ assign curPipReadyToSend =
     isAdd | isSub | isXor | isOr | isAnd | isCmpLessThanSign | isCmpLessThanUSign
 )) |
 (((pipState == shiftLeftReg) | (pipState == shiftRightReg) | (pipState == shiftRightArReg)) &
-    r2_val <= 1
+    r2_val[4:0] <= 1
 ) |
 ((pipState == simpleExec) & ( isJmpUopUse | isLdPcUopUse)) |
 ( (pipState == ldstReg) & (isLsUopUse) & (mem_readFin))|
 (pipState == waitSendState);
 
-wire cmpLtSign = (r1_val[XLEN-1] & (~r2_val[XLEN-1])) & 
+wire cmpLtSign = (r1_val[XLEN-1] & (~r2_val[XLEN-1])) | 
                  ((r1_val[XLEN-1] == r2_val[XLEN-1]) & (r1_val[XLEN-2: 0] < r2_val[XLEN-2: 0]));
 
-wire cmpLtUnSign = r1_val[XLEN-1] < r2_val[XLEN-1];
+wire cmpLtUnSign = r1_val < r2_val;
 
 
 
@@ -198,7 +202,7 @@ always @(posedge clk ) begin
                    ((
                     (pipState == shiftLeftReg   ) | 
                     (pipState == shiftRightReg  ) |
-                    (pipState == shiftRightArReg)) & ( r2_val[4:0] == 1)) | 
+                    (pipState == shiftRightArReg)) & ( r2_val[4:0] <= 1)) | 
                     ((pipState == ldstReg) & mem_readFin)
                     ) begin
                         /////// in shift mode
@@ -245,15 +249,15 @@ always @(*) begin
             if (bp_idx == r1_idx)begin
                 r1_write_val = bp_val;
             end else begin
-                r1_write_val = reg1_readData; /// read from memory
+                r1_write_val = regFile1_readData; /// read from memory
             end
 
         end
         
     end else begin
         r1_write_valid <= 0;
-        r1_write_val   <= 0;
         r1_write_en    <= 0;
+        r1_write_val   <= 0;
     end
     
 end
@@ -274,7 +278,7 @@ always @(*) begin
             if (bp_idx == r2_idx)begin
                 r2_write_val = bp_val;
             end else begin
-                r2_write_val = reg2_readData; /// read from memory
+                r2_write_val = regFile2_readData; /// read from memory
             end
 
         end
@@ -283,13 +287,15 @@ always @(*) begin
                     (pipState == shiftRightArReg)) & (r2_val[4:0] > 1))begin
 
                     r2_write_valid = 1;
-                    r2_write_val   = {r2_val[XLEN-1: 5], r2Reduce};
                     r2_write_en    = 1;
+                    r2_write_val   = {r2_val[XLEN-1: 5], r2Reduce};
+
                     
     end else begin
         r2_write_valid = 0;
-        r2_write_val   = 0;
         r2_write_en    = 0;
+        r2_write_val   = 0;
+        
     end
     
 end
@@ -302,21 +308,22 @@ always @(*) begin
     wb_valid      = 0;
     wb_idx        = 0;
     wb_val        = 0;
-    wb_en_meta    = 0;
+    wb_en_valid   = 0;
+    wb_en_idx     = 0;
     wb_en_data    = 0;
     mem_writeData = 0;
 
     if (pipState == regAccess) begin
-        wb_valid   = rd_valid;
-        wb_idx     = rd_idx;
-        wb_val     = rd_val;
-        wb_en_meta = 1;
-        wb_en_data = 1;
+        wb_valid    = rd_valid;
+        wb_idx      = rd_idx;
+        wb_val      = rd_val;
+        wb_en_valid = 1;
+        wb_en_idx   = 1;
+        wb_en_data  = 1;
     end else if (pipState == simpleExec) begin
 
         if (isAluUopUse) begin
-            wb_valid  = rd_valid;
-            wb_en_data = 1;                
+            wb_valid   = 1;
             if (isAdd)               begin wb_val = r1_val + r2_val; end          
             if (isSub)               begin wb_val = r1_val - r2_val; end          
             if (isXor)               begin wb_val = r1_val ^ r2_val; end          
@@ -328,38 +335,43 @@ always @(*) begin
                 isShiftRightLogical | 
                 isShiftRightArith)  begin wb_val = r1_val; end
 
+            wb_en_valid = 1;
+            wb_en_data = 1;
+
         end
         
         if (isJmpUopUse & (isJal | isJalR)) begin
-            wb_valid  = rd_valid;
-            wb_en_data = 1;
+            wb_valid  = 1;
             wb_val = pc + 4;
+            wb_en_valid = 1;
+            wb_en_data  = 1;
         end
 
         if (isLdPcUopUse) begin
             wb_valid  = rd_valid;
-            wb_en_data = 1;
             if (isNeedPc) begin
                 wb_val = pc + r2_val;
             end else begin
                 wb_val = r2_val;
             end
+            wb_en_valid = 1;
+            wb_en_data  = 1;
         end
         
     end else if (pipState == shiftLeftReg) begin
         if (r2_val[4:0] > 0)begin
+            wb_val = wb_cur_val << 1;
             wb_en_data = 1;
-            wb_val = wb_val << 1;
         end
     end else if (pipState == shiftRightReg) begin
         if (r2_val[4:0] > 0)begin
+            wb_val = wb_cur_val >> 1;
             wb_en_data = 1;
-            wb_val = wb_val >> 1;
         end
     end else if (pipState == shiftRightArReg) begin
         if (r2_val[4:0] > 0)begin
+            wb_val = { wb_cur_val[XLEN-1],wb_cur_val[XLEN-2:0] >> 1};
             wb_en_data = 1;
-            wb_val[XLEN-2:0] = wb_val[XLEN-2:0] >> 1;
         end
     end else if (pipState == ldstReg) begin
         if ( (ldsize == 2'b00) & mem_readFin) begin ///// 8 bit mode
@@ -393,7 +405,7 @@ always @(*) begin
             wb_val = mem_radData;
 
             if (~isMemLoad)begin
-                mem_writeData    = r2_val;
+                mem_writeData = r2_val;
             end
         end
     end
@@ -405,16 +417,16 @@ end
 always @(*) begin
 
     mem_readEn       = 0;    
-    mem_readAddr     = r1_val + r2_val;      
+    mem_readAddr     = r1_val + r3_val;      
     mem_writeEn      = 0;     
-    mem_writeAddr    = r1_val + r2_val;       
+    mem_writeAddr    = r1_val + r3_val;       
     
 
     if ( pipState ==  ldstReg) begin
-            mem_readEn = 1;
-            if ( (~isMemLoad) & mem_readFin)begin
-                mem_writeEn      = 1;
-            end
+        mem_readEn = 1;
+        if ( (~isMemLoad) & mem_readFin)begin
+            mem_writeEn      = 1;
+        end
     end
     
 end
@@ -422,14 +434,14 @@ end
 assign misPredict = ((pipState == simpleExec) && isJmpUopUse) & (
 isJalR         |         
 isJal          |        
-(isEq    &  (r1_val == r2_val))           | (isNEq   &  (r1_val != r2_val))              | 
-(isLt    &  (jumpExtendMode & cmpLtSign)) | (isGe    &  (jumpExtendMode & ~(cmpLtSign))) |
-(isLt    &  ((~jumpExtendMode) &   cmpLtUnSign)) | (isGe    &  ((~jumpExtendMode) & (~cmpLtUnSign)))
+(isEq &  (r1_val == r2_val)               ) | (isNEq   &  (r1_val != r2_val)                  ) | 
+(isLt &  (jumpExtendMode & cmpLtSign)     ) | (isGe    &  (jumpExtendMode & ~(cmpLtSign))     ) |
+(isLt &  ((~jumpExtendMode) & cmpLtUnSign)) | (isGe    &  ((~jumpExtendMode) & (~cmpLtUnSign)))
 );
 
 assign reqPc = ((pipState == simpleExec) && isJmpUopUse) ?
-                 (isJal  ? pc + r2_val :
+                 (isJal  ? pc     + r2_val :
                   isJalR ? r1_val + r2_val :
-                           pc + r3_val) : 0;
+                           pc     + r3_val): 0;
 
 endmodule
